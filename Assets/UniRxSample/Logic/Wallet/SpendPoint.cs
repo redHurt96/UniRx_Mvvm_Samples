8262f0c7b57356700bc9b9db18605e6d32d9455e
redHurt <62645756+redHurt96@users.noreply.github.com>
2024-01-09 00:19:32 +0300

    upd

--- Assets/UniRxSample/Logic/Wallet/SpendPoint.cs
+++ Assets/UniRxSample/Logic/Wallet/SpendPoint.cs
@@ -1,6 +1,6 @@
+using System;
+using UniRx;
 using UnityEngine;
-using static UnityEngine.Mathf;
-using static UnityEngine.Time;
 
 namespace UniRxSample
 {
@@ -8,36 +8,27 @@ namespace UniRxSample
     {
         private const float SPEND_TIME = .5f;
         
-        private bool _canSpend;
         private float _spendCooldown;
         private Model _model;
+        private CompositeDisposable _disposable = new();
 
         private void OnTriggerEnter(Collider other)
         {
             if (other.TryGetComponent(out CharacterView _))
-                _canSpend = true;
+            {
+                Observable
+                    .Interval(TimeSpan.FromSeconds(SPEND_TIME))
+                    .Subscribe(_ => _model.MoveToWallet())
+                    .AddTo(_disposable = new());
+            }
         }
 
         private void OnTriggerExit(Collider other)
         {
             if (other.TryGetComponent(out CharacterView _))
-                _canSpend = false;
+                _disposable.Dispose();
         }
-
-        private void Update()
-        {
-            if (!_canSpend)
-                return;
-
-            _spendCooldown = Max(_spendCooldown - deltaTime, 0f);
-
-            if (Approximately(_spendCooldown, 0f) && _model.Coins.Value > 0)
-            {
-                _model.MoveToWallet();
-                _spendCooldown = SPEND_TIME;
-            }
-        }
-
+        
         public void SetupModel(Model model) => 
             _model = model;
     }
